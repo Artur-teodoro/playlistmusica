fdbd9c6f2ebe7a912f0cbf72b20e7f7c90347b39
Artur <arturfreitas0909@gmail.com>
2024-10-08 09:55:17 -0300

    first commit

---
+++ playlist/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    capa = models.ImageField(upload_to='capas/',validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])])
+
+    def __str__(self):
+        return f"{self.titulo}"
+    
+class Playlist(models.Model):
+    nome = models.CharField(max_length=100)
+    filme = models.ManyToManyField(Filme)
+
+    def __str__(self):
+        return self.nome
